9c603e3c4836aad2f6011c085a5ebae4fcc24da3
maxine-ellah <email": null>
2016-05-03 10:04:06 +1200

    add github call for user info

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,12 @@
 var React = require('react');
-
+function puke(object){
+  return <pre>{JSON.stringify(object,null, ' ')}</pre>
+}
 function ConfirmBattle (props) {
   //if the component is loading, render loading, otherswise render confirm battle
   return props.isLoading ===true
     ? <p>LOADING!</p>
-    : <p>CONFIRM BATTLE!</p>
+    : <p>CONFIRM BATTLE!: {puke(props)}</p>
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,6 @@
 var React = require('react')
 var ConfirmBattle = require('../components/ConfirmBattle');
-
+var githubHelpers = require('../utils/githubHelpers')
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -16,6 +16,14 @@ var ConfirmBattleContainer = React.createClass({
   componentDidMount: function(){
     var query = this.props.location.query  //routed over as props from the PromptContainer
     //fetch infor from gh then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players){
+        this.setState({
+          isLoading:false,
+          playersInfo:[players[0], players[1]]
+        })
+      }.bind(this))
+
   },
 
   render: function() {
