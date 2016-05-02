83cf1fe49d483eabbd082732511f6b6b2e7a1549
maxine-ellah <email": null>
2016-05-03 10:57:34 +1200

    add ui componenets in ConfirmBattle via UserDetails/Wrapper

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -10,7 +10,7 @@ var ConfirmBattleContainer = React.createClass({
   getInitialState: function(){
     return {
       isLoading: true,
-      playerInfo: []
+      playersInfo: []
     }
   },
   componentDidMount: function(){
@@ -26,11 +26,22 @@ var ConfirmBattleContainer = React.createClass({
 
   },
 
+  handleInitiateBattle: function(){
+    this.context.router.push({
+      pathname: '/results',
+      state:{
+        playersInfo: this.state.playersInfo
+      }
+    })
+
+  },
+
   render: function() {
     return(
       <ConfirmBattle
         isLoading={this.state.isLoading}
-        playersInfo={this.state.playersInfo}/>
+        playersInfo={this.state.playersInfo}
+        onInitiateBattle={this.handleInitiateBattle}/>
     );
   }
 
